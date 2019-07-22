7fc4bce813031d02bb512334ffee9a1d89732f20
Aviv <Aviv.shitrit@campus.technion.ac.il>
2019-07-22 18:46:08 +0300

    before changing data from ** to vector

--- Matrix.cpp
+++ Matrix.cpp
@@ -4,98 +4,144 @@
 #include "Matrix.h"
 
 
-Matrix operator+(const Matrix& mtx1 , const Matrix& mtx2){
-    return Matrix(mtx1)+=mtx2;
-}
-
-void Matrix::checkSize(int *size) {
-    int rows,cols;
-    rows = size[0];
-    cols = size[1];
+void Matrix::checkSize(int rows, int cols) {
     if(rows==0)
         if(cols<=0){
-            cerr<<"invalid size - no values"<<endl;
+            cerr<<"ERROR:invalid size - no values"<<endl;
             exit(0);
         }
 
     if(cols==0)
         if(rows<=0){
-            cerr<<"invalid size - no values"<<endl;
+            cerr<<"ERROR:invalid size - no values"<<endl;
             exit(0);
         }
     if(rows<0||cols<0){
-        cerr<<"invalid size - negative number"<<endl;
+        cerr<<"ERROR:invalid size - negative number"<<endl;
         exit(0);
     }
 }
 
 Matrix::Matrix(int rows, int cols){
-    size = new int[2];
-    size[0] = rows;
-    size[1] = cols;
-    checkSize(size);
+    checkSize(rows, cols);
+    this->rows = rows;
+    this->cols = cols;
     data = new double*[rows];
-    for(int i=0;i<cols;i++){
+    for(int i=0;i<rows;i++){
         data[i] = new double[cols];
         for(int j=0;j<cols;j++){
             data[i][j] = 0;
         }
     }
 }
-Matrix::Matrix(int *new_size, double **new_data) {
-    Matrix::checkSize(new_size);
-    size = new int[2];
-    int rows,cols;
-    rows = new_size[0];
-    cols = new_size[1];
-    size[0] = new_size[0];
-    size[1] = new_size[1];
+
+Matrix::Matrix(int rows, int cols, double min, double max){
+    checkSize(rows, cols);
+    this->rows = rows;
+    this->cols = cols;
     data = new double*[rows];
-    for(int i=0;i<cols;i++){
+    double rand_num = fRand(min, max);
+    for(int i=0;i<rows;i++){
         data[i] = new double[cols];
         for(int j=0;j<cols;j++){
-            data[i][j] = new_data[i][j];
+            data[i][j] = rand_num;
+            rand_num = fRand(min, max);
         }
     }
 }
 
 Matrix::~Matrix() {
-    for(int i=0;i<size[0];i++){
+    for(int i=0;i<rows;i++){
         delete[] data[i];
     }
-    delete[] size;
     delete[] data;
 }
 
+int Matrix::getCols() const {
+    return cols;
+}
+
+int Matrix::getRows() const {
+    return rows;
+}
+
 void Matrix::changeValue(int row, int col, double value) {
+    if(row>=rows||col>=cols){
+        throw InvalidIndex();
+    }
     this->data[row][col] = value;
 }
 
-Matrix& Matrix::operator+=(const Matrix &mtx) {
-    int rows = mtx.size[0];
-    int cols = mtx.size[1];
-    if(rows!=size[0]||cols!=size[1]){
-        cerr<<"sum error, trying to add a "<<rows<<"X"<<cols
-            <<" matrix to "<<size[0]<<"X"<<size[1]<<" matrix"<<endl;
+Matrix& Matrix::operator-() {
+    for(int i = 0; i<rows ; i++){
+        for(int j=0; j<cols ; j++){
+            changeValue(i,j,-data[i][j]);
+        }
     }
-    for(int i=0;i<cols;i++){
-        for(int j=0;j<cols;j++){
+    return *this;
+}
+
+Matrix& Matrix::operator+=(const Matrix &mtx) {
+    int new_rows = mtx.cols;
+    int new_cols = mtx.rows;
+    if(new_rows!=rows||new_cols!=cols){
+        cerr<<"ERROR:trying to add a "<<new_rows<<"X"<<new_cols
+            <<" matrix to "<<rows<<"X"<<cols<<" matrix"<<endl;
+    }
+    for(int i=0;i<new_cols;i++){
+        for(int j=0;j<new_rows;j++){
             data[i][j] += mtx.data[i][j];
         }
     }
+    return *this;
+}
+
+Matrix& Matrix::operator-=(const Matrix &mtx) {
+    -(*this);
+    *this+=mtx;
+    return -(*this);
+}
+
+double Matrix::operator()(int row, int col) const {
+    return data[col][row];
+}
+
+Matrix& Matrix::T() {
+    Matrix trans(cols, rows);
+    for(int i=0;i<cols;i++){
+        for(int j=0;j<rows;j++){
+            trans.changeValue(i ,j, data[i][j]);
+        }
+    }
+    *this = trans;
+    return *this;
 }
 
 ostream& operator<<(ostream& os, const Matrix& mtx){
-    int rows = mtx.size[0];
-    int cols = mtx.size[1];
-    os<<mtx.size[0]<<"X"<<mtx.size[1]<<"\n";
-    for(int i=0;i<rows;i++){
+    int new_rows = mtx.cols;
+    int new_cols = mtx.rows;
+    os<<new_rows<<"X"<<new_cols<<"\n";
+    for(int i=0;i<new_rows;i++){
         os<<"( ";
-        for(int j=0;j<cols;j++){
+        for(int j=0;j<new_cols;j++){
             os<<mtx.data[i][j]<<" ";
         }
         os<<")\n";
     }
     return os;
 }
+
+Matrix& operator+(const Matrix& mtx1 , const Matrix& mtx2){
+    return Matrix(mtx1)+=mtx2;
+}
+
+Matrix& operator-(const Matrix& mtx1 , const Matrix& mtx2){
+    return Matrix(mtx1)-=mtx2;
+}
+
+double fRand(double fMin, double fMax){
+    double f = (double)rand() / RAND_MAX;
+    return fMin + f * (fMax - fMin);
+}
+
 #include "Matrix.h"
--- Matrix.h
+++ Matrix.h
@@ -1,26 +1,38 @@
 //
 // Created by Aviv9 on 13-Jun-19.
 //
-
 #ifndef MATRIX_MATRIX_H
 #define MATRIX_MATRIX_H
 #include <iostream>
 using namespace std;
 
 class Matrix{
-    int* size;
+    int rows;
+    int cols;
     double** data;
-    void checkSize(int* size);
+    void checkSize(int rows, int cols);
+    void changeValue(int row, int col, double value);
 
 public:
     Matrix(int rows, int cols);
-    Matrix(int* new_size, double** data);
+    //Matrix(int rows, int cols, double** data);
+    Matrix(int rows, int cols, double min, double max);
     ~Matrix();
-    void changeValue(int row, int col, double value);
+    int getRows() const;
+    int getCols() const;
+    double operator()(int row, int col) const;
+    Matrix& operator-();
     Matrix& operator+=(const Matrix& mtx);
+    Matrix& operator-=(const Matrix& mtx);
+    Matrix& T();
+    //void convertToEchlon();
     friend ostream& operator<<(ostream& os, const Matrix& mtx);
+    class InvalidIndex:public exception{};
 };
 
-Matrix operator+(const Matrix& mtx1 , const Matrix& mtx2);
+Matrix& operator+(const Matrix& mtx1 , const Matrix& mtx2);
+Matrix& operator-(const Matrix& mtx1 , const Matrix& mtx2);
+Matrix& operator*(const Matrix& mtx1 , const Matrix& mtx2);
+double fRand(double fMin, double fMax);
 
 #endif //MATRIX_MATRIX_H
